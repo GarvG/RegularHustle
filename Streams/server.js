5d6eda9153bf4b18ae568093188dd20ad6e6d791
GarvG <67434601+GarvG@users.noreply.github.com>
2023-03-27 23:10:36 +0530

    Streams

--- Streams/server.js
+++ Streams/server.js
@@ -2,10 +2,24 @@ const http=require('http');
 const fs=require('fs');
 const server=http.createServer((req,res)=>{
 console.log('request coming');
-const file=fs.readFileSync('sample')
+// requesting file from server if there is a big file, so it is bad way!
+
+// const file=fs.readFileSync('sample.txt');
+// return res.end(file);
+ 
+// requesting file from server if there is a big file, so it is good way! using streams
+
+// const readablestream=fs.createReadStream('sample.txt');
+// readableStram-->pipe-->writeableStream(response)
+// the data from sample text will come chunk by chunk it go in buffer then through pipe we get it in writeable stream
+// readablestream.pipe(res);
+
+
+
 });
 
 
 server.listen(3000,()=>{
     console.log('server listening on port 3000');
 })
+
